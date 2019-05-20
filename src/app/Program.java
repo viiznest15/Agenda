939fee29a0377debf0afa4e9be43cf8e5c42a20b
Evans <evansg.caso91@gmail.com>
2019-05-20 13:56:28 +0200

    Pruebas Calendar

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,8 +1,30 @@
 package app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		for (String m : new String[] {"2019-01", "2019-02", "2019-03",
+	              "2019-04", "2019-05", "2019-06", "2019-07", "2019-08",
+	              "2019-09", "2019-10", "2019-11", "2019-12"}) {
+	      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+	      Date date = format.parse(m);
+	      Calendar c = Calendar.getInstance();
+	      c.setTime(date);
+
+	      int start = c.get(Calendar.WEEK_OF_MONTH);
+
+	      c.add(Calendar.MONTH, 1);
+	      c.add(Calendar.DATE, -1);
+	      int end = c.get(Calendar.WEEK_OF_MONTH);
+	      System.out.println(" # of weeks in " + format.format(c.getTime())
+	              + ": " + (end - start + 1));
+	  }
 
 	}
 }
