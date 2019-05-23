6ed040f7b7719a9820427b60dd4373c4faf505d4
Victor <viiznest15@gmail.com>
2019-05-23 13:37:00 +0200

    Creación clase "Validator"
    - Se crea la clase "validator" para validar las peticiones teniendo en cuenta el mes que nos entra por "config.txt".
    - fix archivos de idiomas.
    - Añade en DateTools: método para conseguir mes de fecha.

--- src/services/ServiceManagementLonge.java
+++ src/services/ServiceManagementLonge.java
@@ -5,36 +5,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import domain.Config;
 import domain.Reservation;
+import domain.Validator;
 
 public class ServiceManagementLonge {
 
-	public Map<String, List<Reservation>> getSalasConReservas(String nomFichero) {
+	public Map<String, List<Reservation>> getSalasConReservas(String nomFichero, Config config) {
 
 		ServiceReservCreation serv = new ServiceReservCreation();
 
 		// El map nos guarda los distintos valores de sala del fichero
-		Map<String, String> salasDist = serv.getReservFromFile(nomFichero);
+		Map<String, String> diffLounges = serv.getReservFromFile(nomFichero);
 
 		// La lista guarda esos valores de forma mas simple, teniendo asi un campo por
 		// sala
-		List<String> salasList = new ArrayList(salasDist.keySet());
+		List<String> loungesList = new ArrayList<String>(diffLounges.keySet());
 
 		// Cargamos en reservasList todas las reservas del fichero
-		List<Reservation> reservasList = serv.getReservListFromFile(nomFichero);
+		List<Reservation> reservList = serv.getReservListFromFile(nomFichero);
 
 		// lista temporal para gestionar las listas dentro del map
 		List<Reservation> temp = new ArrayList<Reservation>();
-		Map<String, List<Reservation>> reservas = new HashMap<>();
-		for (String sala : salasList) {
-			for (Reservation reserva : reservasList) {
-				if (reserva.getLounge().equals(sala)) {
-					temp.add(reserva);
+		
+		// Se crea el objeto validator par vereficar las fechas de las peticiones que entran al programa.
+		Validator validator = new Validator();
+		
+		Map<String, List<Reservation>> reservations = new HashMap<>();
+		for (String lounge : loungesList) {
+			for (Reservation reserv : reservList) {
+				if (reserv.getLounge().equals(lounge) && validator.validateMonth(reserv, config)) {
+					temp.add(reserv);
 				}
 			}
-			reservas.put(sala, new ArrayList<Reservation>(temp));
+			reservations.put(lounge, new ArrayList<Reservation>(temp));
 			temp.clear();
 		}
-		return reservas;
+		return reservations;
 	}
 }
